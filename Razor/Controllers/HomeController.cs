f27d5e6b75fd6291c60b74804485f0dbdef513ae
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-05-01 13:27:29 +0200

    Add project files.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kajak",
+                Description = "Jednoosobowa łódka",
+                Category = "Sporty wodne",
+                Price = 275M
+            };
+
+            return View(myProduct);
+        }
+    }
+}
